c2561589ba3c288615bea73ac2f3f5bf2a197f09
vokrob <106639488+vokrob@users.noreply.github.com>
2024-11-26 21:25:36 +0700

    Adding a Fine location resolution check

--- app/src/main/java/com/vokrob/weather_forecast/fragments/MainFragment.kt
+++ app/src/main/java/com/vokrob/weather_forecast/fragments/MainFragment.kt
@@ -1,13 +1,18 @@
 package com.vokrob.weather_forecast.fragments
 
+import android.Manifest
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import com.vokrob.weather_forecast.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
+    private lateinit var pLauncher: ActivityResultLauncher<String>
     private lateinit var binding: FragmentMainBinding
 
     override fun onCreateView(
@@ -16,6 +21,25 @@ class MainFragment : Fragment() {
     ): View {
         binding = FragmentMainBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        checkPermission()
+    }
+
+    private fun permissionListener() {
+        pLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+            Toast.makeText(activity, "Permission is $it", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun checkPermission() {
+        if (!isPermissionGranted(Manifest.permission.ACCESS_FINE_LOCATION)) {
+            permissionListener()
+            pLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        }
     }
 
     companion object {
