09471bd78e566f70f971070dea7bf9fb309097cd
vokrob <106639488+vokrob@users.noreply.github.com>
2024-11-28 22:06:30 +0700

    Check RecyclerView Adapter

--- app/src/main/java/com/vokrob/weather_forecast/adapters/WeatherAdapter.kt
+++ app/src/main/java/com/vokrob/weather_forecast/adapters/WeatherAdapter.kt
@@ -1,11 +1,16 @@
 package com.vokrob.weather_forecast.adapters
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.vokrob.weather_forecast.R
 import com.vokrob.weather_forecast.databinding.ListItemBinding
 
-class WeatherAdapter : ListAdapter<WeatherModel, WeatherAdapter.Holder>() {
+class WeatherAdapter : ListAdapter<WeatherModel, WeatherAdapter.Holder>(Comparator()) {
+
     class Holder(view: View) : RecyclerView.ViewHolder(view) {
         val binding = ListItemBinding.bind(view)
 
@@ -14,6 +19,25 @@ class WeatherAdapter : ListAdapter<WeatherModel, WeatherAdapter.Holder>() {
             tvCondition.text = item.condition
             tvTemp.text = item.currentTemp
         }
+    }
+
+    class Comparator : DiffUtil.ItemCallback<WeatherModel>() {
+        override fun areItemsTheSame(oldItem: WeatherModel, newItem: WeatherModel): Boolean {
+            return oldItem == newItem
+        }
+
+        override fun areContentsTheSame(oldItem: WeatherModel, newItem: WeatherModel): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
+        return Holder(view)
+    }
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        holder.bind(getItem(position))
     }
 }
 
--- app/src/main/java/com/vokrob/weather_forecast/fragments/HoursFragment.kt
+++ app/src/main/java/com/vokrob/weather_forecast/fragments/HoursFragment.kt
@@ -5,16 +5,52 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.vokrob.weather_forecast.R
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.vokrob.weather_forecast.adapters.WeatherAdapter
+import com.vokrob.weather_forecast.adapters.WeatherModel
+import com.vokrob.weather_forecast.databinding.FragmentHoursBinding
 
 class HoursFragment : Fragment() {
+    private lateinit var binding: FragmentHoursBinding
+    private lateinit var adapter: WeatherAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_hours, container, false)
+    ): View {
+        binding = FragmentHoursBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initRcView()
+    }
+
+    private fun initRcView() = with(binding) {
+        rcView.layoutManager = LinearLayoutManager(activity)
+        adapter = WeatherAdapter()
+        rcView.adapter = adapter
+
+        val list = listOf(
+            WeatherModel(
+                "", "12:00",
+                "Sunny", "25°C",
+                "", "", "", ""
+            ),
+            WeatherModel(
+                "", "13:00",
+                "Sunny", "27°C",
+                "", "", "", ""
+            ),
+            WeatherModel(
+                "", "14:00",
+                "Sunny", "35°C",
+                "", "", "", ""
+            ),
+        )
+        adapter.submitList(list)
     }
 
     companion object {
@@ -22,6 +58,19 @@ class HoursFragment : Fragment() {
         fun newInstance() = HoursFragment()
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
