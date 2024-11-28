4d111cba706f451e7635930ff26d6058b0ddd203
vokrob <106639488+vokrob@users.noreply.github.com>
2024-11-28 20:36:29 +0700

    Creating WeatherModel data class and item markup for list_item

--- app/src/main/java/com/vokrob/weather_forecast/MainViewModel.kt
+++ app/src/main/java/com/vokrob/weather_forecast/MainViewModel.kt
@@ -2,10 +2,11 @@ package com.vokrob.weather_forecast
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.vokrob.weather_forecast.adapters.WeatherModel
 
 class MainViewModel : ViewModel() {
-    val liveDataCurrent = MutableLiveData<String>()
-    val liveDataList = MutableLiveData<List<String>>()
+    val liveDataCurrent = MutableLiveData<WeatherModel>()
+    val liveDataList = MutableLiveData<List<WeatherModel>>()
 }
 
 
